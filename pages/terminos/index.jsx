b83b638e299df2578e3326e64b168f606d1761c6
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-09-06 21:14:00 -0500

    s

--- pages/terminos/index.jsx
+++ pages/terminos/index.jsx
@@ -1,1 +1,18 @@
 import NavbarLogin from "@/components/NavbarLogin";
+
+const Terminos = () => {
+    return (
+      <div>
+        {/* className={styles.} */}
+        <header className={styles.header}>
+          <Link href="/" className={styles.link__header}>
+            <div className={styles.logo}>
+              <Image className={styles.img} src="/img/logo_blanco.png" alt="Logo Símbolo SENA y Biometric Service Blanco" width={469} height={209}/>
+            </div>
+          </Link>
+        </header>
+      </div>
+    );
+  };
+  
+  export default NavbarLogin;
