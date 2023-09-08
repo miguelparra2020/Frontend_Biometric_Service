d729585202c60140a43070a32919f2bad812cd2d
Sebastian Bocanegra <sebasbocanegra17@gmail.com>
2023-09-08 10:28:14 -0500

    Terminos

--- pages/terminos/index.jsx
+++ pages/terminos/index.jsx
@@ -4,9 +4,14 @@ const Terminos = () => {
     return (
       <div>
         {/* className={styles.} */}
-    hola
+        <main className="main">
+          <h1>Téminos y condiciones</h1>
+          <p>
+          Apreciado usuario, el sitio web institucional del Servicio Nacional de Aprendizaje – SENA - tiene como función principal proveer, divulgar y gestionar información sobre los productos, programas, trámites y servicios de la institución. Por medio de su sitio www.sena.edu.co el Servicio Nacional de Aprendizaje – SENA - publica los temas y actividades que tienen que ver con su misión, su visión, objetivos y las funciones que corresponden a su objeto social. Al igual que los productos, servicios, programas y en general información relacionada con la gestión que adelanta. El Servicio Nacional de Aprendizaje – SENA - no persigue ningún lucro, ganancia o interés comercial con los contenidos y enlaces que se publican en su sitio web institucional y en los sitios web de los diferentes productos y servicios que son parte de la entidad y a los cuales se accede a través de www.sena.edu.co.
+          </p>
+        </main>
       </div>
     );
   };
   
-  export default NavbarLogin;
+  export default Terminos;
