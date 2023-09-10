b5d830cf29cd5f7431103a616ccd788318bb2139
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-09 21:21:44 -0500

    Update

--- pages/fichas/[id].js
+++ pages/fichas/[id].js
@@ -1,6 +1,6 @@
 import React from "react";
 import MainLayout from '../../components/layouts/MainLayout';
-
+import '../../styles/pages/ingresos.css';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
@@ -48,24 +48,29 @@ function FichaPage() {
 
     return(
         <MainLayout>
-            <div>
-                <h1>Hola soy el detalle de cada ficha</h1>             
+          <div className='container_botton_volver'>
+            <h3><Link href="/fichas" className="volver_link">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+            </svg> &nbsp;Volver</Link></h3>
+        </div>
+            <div className="titulo-ingresos">
+                <h1>Editar ficha</h1>
             </div>
-            <div>
-            <h3><Link href="/fichas">Volver</Link></h3>
-            <h1>Editar ficha</h1>   
+            <form  className="container">
             <label>
                 Número de la ficha:
-                <input type="text" value={numero_ficha} onChange={e => setNumeroFicha(e.target.value)} />
+                <input type="text" value={numero_ficha} onChange={e => setNumeroFicha(e.target.value)} className="inputs-ingresos" required/>
             </label>
             <label>
                 Nombre de la ficha:
-                <textarea value={nombre_ficha} onChange={e => setNombreFicha(e.target.value)} />
+                <textarea value={nombre_ficha} onChange={e => setNombreFicha(e.target.value)} className="inputs-ingresos" required/>
             </label>
             
             <button onClick={handleUpdate}>Guardar cambios</button> 
+            &nbsp;&nbsp;&nbsp;
             <button onClick={handleDelete}>Eliminar</button> 
-            </div>
+            </form> 
         </MainLayout>
     )
 
