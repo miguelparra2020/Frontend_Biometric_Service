8b9142f3f09a88a67f4c9cdb292f9f738273883a
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-12 10:22:10 -0500

    Update

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -147,15 +147,7 @@ const NavbarMain = () => {
                 </svg>            
           </div>
         </Link>
-        <Link href="/miperfil" className={styles.links}> 
-          <div className={styles.nav__link}>                
-                Mi Perfil &nbsp;
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
-  <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
-  <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                </svg>           
-          </div>
-        </Link>
+        {tipo_usuario !== "aprendiz" && tipo_usuario !== '' && (
         <Link href="/fichas" className={styles.links}>  
           <div className={styles.nav__link}>
                 Fichas &nbsp;
@@ -163,7 +155,8 @@ const NavbarMain = () => {
   <path d="M8.235 1.559a.5.5 0 0 0-.47 0l-7.5 4a.5.5 0 0 0 0 .882L3.188 8 .264 9.559a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882L12.813 8l2.922-1.559a.5.5 0 0 0 0-.882l-7.5-4zM8 9.433 1.562 6 8 2.567 14.438 6 8 9.433z"/>
                 </svg>       
           </div>
-        </Link>
+        </Link>)}
+        {tipo_usuario !== "aprendiz" && tipo_usuario !== '' && (
         <Link href="/usuarios" className={styles.links}>  
           <div className={styles.nav__link}>
             Usuarios &nbsp;
@@ -171,7 +164,16 @@ const NavbarMain = () => {
   <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
             </svg>
           </div>               
+        </Link>)}
+        <Link href="/miperfil" className={styles.links}>   
+          <div className={styles.nav__link}>            
+              Más &nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+</svg>         
+          </div>
         </Link>
+
         <Link href="/" className={styles.links}>   
           <div className={styles.nav__link}>            
               Salir &nbsp;
