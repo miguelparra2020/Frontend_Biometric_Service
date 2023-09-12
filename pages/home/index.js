38f9ccfcd8934850448ba69b4a72655372b13383
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-12 13:10:02 -0500

    Update

--- pages/home/index.js
+++ pages/home/index.js
@@ -94,6 +94,11 @@ const realizarBusqueda = () => {
     
   };
 
+  const recargarBusqueda = () => {
+        setBusquedaUsuario('')
+        router.push('/home');
+  }
+
 
 
 
@@ -267,9 +272,16 @@ const realizarBusqueda = () => {
                             value={busquedaUsuario}
                             onChange={(e) => {
                                 setBusquedaUsuario(e.target.value);
+                                
                             }}
                             />&nbsp; 
-                            <button onClick={realizarBusqueda} className="boton_busqueda">Buscar</button>   
+                            <button onClick={realizarBusqueda} className="boton_busqueda">Buscar <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg></button>&nbsp;    
+                            <button onClick={recargarBusqueda} className="boton_busqueda">Recargar <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+  <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+</svg></button>   
                     </div>
                 </div>)}
                 {/* busqueda por usuario */}
