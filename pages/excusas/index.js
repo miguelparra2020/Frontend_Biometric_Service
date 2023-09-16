e318ef6e1ae9e0e1e3464cf57a137e0b42d3d840
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-16 17:25:53 -0500

    Update

--- pages/excusas/index.js
+++ pages/excusas/index.js
@@ -65,14 +65,38 @@ function ExcusasPage(){
                 tipo_usuario !== 'aprendiz' ? (<div>
                     {excusas.map((item) => (
                             <div key={item.id} className="card_excusa">
-                                <div>
-                                    <div></div>
-                                    <div></div>
-                                    <div></div>
+                                <div className="header_card_excusa"> 
+                                    <div><Image src={item.imagen_perfil} width={50} height={50} alt="Imagen de perfil" className="imagen_perfil_card_excusa"/></div>
+                                    &nbsp;&nbsp;
+                                    <div><strong>Id excusa:</strong>  {item.id} </div>
+                                    &nbsp;&nbsp;
+                                    <div><Image src="https://res.cloudinary.com/unidigital/image/upload/v1694901295/biometric%20services/historial-medico_ekakxl.png" width={40} height={40} alt="Imagen de excusa"/></div>
                                 </div>
-                                Id excusa: {item.id}
+                                <br/>
+                                <div className="fila_card_excusa">
+                                    <div>◽<strong>Usuario:</strong> {item.username}</div>&nbsp;&nbsp;
+                                    <div><strong>Nombre:</strong>  {item.first_name}  {item.last_name}</div>
+                                </div>
+                                <div className="fila_card_excusa">
+                                    <div>◽<strong>Ficha:</strong> {item.numero_ficha}</div>&nbsp;&nbsp;
+                                    <div><strong>Tipo:</strong>  {item.first_name}  {item.tipo_usuario}</div>
+                                </div>
+                                <div className="fila_card_excusa">
+                                    <div>◽<strong>Comentario del aprendiz:</strong> {item.comentario_aprendiz}</div>
+                                </div>
+                                <br/>
+                                <div className="fila_card_excusa">
+                                    <div><strong>Archivo Excusa:</strong> 
+                                        <Link href={item.archivo_excusa} target="_blank"> <Image src="https://res.cloudinary.com/unidigital/image/upload/v1694902849/biometric%20services/pdf_juwlld.png" width={30} height={30} alt="Imagen de pdf"/></Link>
+                                    </div>
+                                </div>
+                                <br/>
+                                <br/>
+                                <div className="fila_card_excusa">
+                                    <div>◽<strong>Estado:</strong> {item.estado_excusa}</div>
+                                </div>
 
-                                {item.username}                                
+                                                       
                             </div>
                         ))}
                 </div>) : (<></>)
