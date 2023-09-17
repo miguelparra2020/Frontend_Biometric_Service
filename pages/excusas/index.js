75942cfbd05cc84585300ba26a124114c908272c
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-17 10:46:16 -0500

    Update

--- pages/excusas/index.js
+++ pages/excusas/index.js
@@ -14,6 +14,7 @@ function ExcusasPage(){
     const [tipo_usuario, setTipoUsuario] = useState('');
     const [excusas, setExcusas] = useState([]);
     const [excusasAprendiz, setExcusasAprendiz] = useState([]);
+    const [estadoFiltrado, setEstadoFiltrado] = useState("pendiente"); 
     const router = useRouter();
 //----------------Variables---------------------------------
 
@@ -63,7 +64,7 @@ function ExcusasPage(){
     }, [access_token,router]);
 //----Función useEffect asyncrona para obtener la data de fichas-------
 
-
+    const excusasFiltradas = excusas.filter(item => item.estado_excusa === estadoFiltrado);
 //---área visual de la página---------
     return (
         <MainLayout>
@@ -139,7 +140,17 @@ function ExcusasPage(){
             <div className="contenedor_excusas">
             { 
                 tipo_usuario !== 'aprendiz' ? (<div>
-                    {excusas.map((item) => (
+                    <div className="fila_botones">
+                    <button onClick={() => setEstadoFiltrado("pendiente")}className="boton_pendiente">Pendientes <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg></button>&nbsp;&nbsp;&nbsp;
+                    <button onClick={() => setEstadoFiltrado("aprobada")}className="boton_aprobada">Aprobadas <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg></button>&nbsp;&nbsp;&nbsp;
+                    <button onClick={() => setEstadoFiltrado("anulada")}className="boton_anulada">Anuladas <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
+</svg></button></div>
+                    {excusasFiltradas.map((item) => (
                             <div key={item.id} className="card_excusa">
                                 <div className="header_card_excusa"> 
                                     <div><Image src={item.imagen_perfil} width={50} height={50} alt="Imagen de perfil" className="imagen_perfil_card_excusa"/></div>
